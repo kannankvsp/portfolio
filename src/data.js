9e393dd62ea32e0fc589bf0fc4274564097f2620
KannanKvsp <kannanpalani69@gmail.com>
2022-09-24 20:28:05 +0530

    added todo project

--- src/data.js
+++ src/data.js
@@ -1,17 +1,18 @@
 import Slampng from './images/slam.png'
 import Schoolpng from './images/school.png'
 import Weatherpng from './images/weather-app.png'
+import TodoPng from './images/todo.png'
 export const skills = [
   "HTML5","CSS3","React js","Node js","MongoDB","MySQL","Python","Scala","Apache Spark","Express js"
 ]
 export const projects = [
   {
-    title: "Weather App",
-    subtitle: "React js",
+    title: "ToDo APP",
+    subtitle: "PWA plus redux",
     description:
-      "Using postal api, it verifies the postal name and generate random weather data",
-    image: Weatherpng,
-    link: "https://kvsp-weather.heroku.com",
+      "todo app with features of description, priority, complete and delete",
+    image: TodoPng,
+    link: "https://hackzeal.me",
   },
   {
     title: "Choose your school",
@@ -20,6 +21,14 @@ export const projects = [
       "collects school data and list for choosing schools",
     image: Schoolpng,
     link: "https://hackzeal.me",
+  },
+  {
+    title: "Weather App",
+    subtitle: "React js",
+    description:
+      "Using postal api, it verifies the postal name and generate random weather data",
+    image: Weatherpng,
+    link: "https://kvsp-weather.heroku.com",
   },
   {
     title: "Slam Book",
